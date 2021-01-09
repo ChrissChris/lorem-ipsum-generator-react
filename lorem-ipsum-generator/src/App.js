630298fedb530253a359f48773a42ea77a57da93
Chriss Chris <zenskech@outlook.com>
2021-01-09 19:53:09 +0500

    Update App.js

--- lorem-ipsum-generator/src/App.js
+++ lorem-ipsum-generator/src/App.js
@@ -1,8 +1,36 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <section className="section-center">
+      <h3>Lorem ipsum</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraph:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+        <article className="lorem-text">
+          {text.map((item, index) => {
+            return <p key={index}>{item}</p>;
+          })}
+        </article>
+      </form>
+    </section>
+  );
 }
 
 export default App;
